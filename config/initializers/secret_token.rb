139e5195c1331e5ebd77040310bb255d23eff2f2
Apoorv Agarwal <apoorv.contact@gmail.com>
2014-02-15 17:51:59 -0600

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FirstApp::Application.config.secret_key_base = '2477a4d08afd4235bc39eab47c29847201b94370de449d78c63fc219cd8b4be7b21c81793360ebf3070f9fdcf346eb33a9ac71ece2364ea8aaf1cb5cde526735'
+FirstApp::Application.config.secret_key_base = ''
